52a7b2eb9217c1a6751ed922b4acaba9088e4f99
KarthikRaveeswaran <348899022@gapps.yrdsb.ca>
2024-10-22 00:10:15 -0400

    Import images

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class InstructionScreen extends World
 {
-
     /**
      * Constructor for objects of class InstructionScreen.
      * 
@@ -17,5 +16,6 @@ public class InstructionScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
     }
 }
